28481842f045329d033d8d76fca26ee9c8e0a5c9
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-03-25 19:20:18 +0100

    Created a popup for input of jobs in todo

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log('Testing typescript!');
+let num = 0;
+function TogglePopup() {
+    let popup = document.querySelector('#popup');
+    num++;
+    if (num == 1) {
+        popup === null || popup === void 0 ? void 0 : popup.classList.add('open-popup');
+    }
+    else {
+        popup === null || popup === void 0 ? void 0 : popup.classList.remove('open-popup');
+        num = 0;
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-//Creating input slot for Todo list
-function AddNewNote(){
-    
+//Creating popup
+let num: number = 0;
+function TogglePopup() {
+    let popup = document.querySelector('#popup');
+    num++;
+    if (num == 1) {
+
+        popup?.classList.add('open-popup');
+    }
+    else {
+        popup?.classList.remove('open-popup');
+        num = 0;
+    }
 }
